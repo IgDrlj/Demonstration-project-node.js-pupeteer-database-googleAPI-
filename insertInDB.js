d445fa8e98d22a84eec791f35df578dcf5fab761
IgDrlj <92441387+IgDrlj@users.noreply.github.com>
2023-11-08 14:18:16 +0100

    Update insertInDB.js

--- insertInDB.js
+++ insertInDB.js
@@ -40,94 +40,10 @@ function insertInDB(storeURLScrape, storeNameFinal, currDealSelector, codes, cur
         db.query(sql, [values], async function (err, result) {
 
           if (err) {
-
             throw err;
-
           } else {
-            var primarySitesCortney = [
-              'Thefarmersdog',
-              'fast-growing-trees',
-              'venus',
-              'bbqguys',
-              'nutrisystem',
-              'saatva',
-              'chewy',
-              'ariat',
-              'rebag',
-              'invictastores',
-              'lovehoney',
-              'greenworkstools',
-              'aroma360',
-              'bluenile',
-              'tjx',
-              'globalgolf',
-              'agjeans',
-              'bouqs',
-              'corkcicle',
-              'storesupply',
-              'aliengearholsters',
-              'journeecollection',
-              'becker',
-              'ashleyblackguru',
-              'grabagun',
-              'safelite',
-              'vicicollection',
-              'store.trimhealthymama',
-              'hvacdirect',
-              'hotelcollection',
-              'lexingtonlaw',
-              'golfballs',
-              'keyzarjewelry',
-              'ticketsmarter',
-              '3balls',
-              'belletire',
-              'scheels',
-              'budgetblinds',
-              'guns',
-              'withclarity',
-              'medterracbd',
-              'naadam',
-              'sundaysfordogs',
-              'carawayhome',
-              'thewebster',
-              'cutterbuck',
-              'surlatable',
-              'picturesongold',
-              'gundrymd',
-              'magnoliabakery',
-              'personalcreations',
-              'thecbdistillery',
-              'frootbat',
-              'storz-bickel',
-              'paulaschoice',
-              'supercuts',
-              'theayurvedaexperience',
-              'aimeekestenberg',
-              'jasemedical',
-              'onnit',
-              'everyplate',
-              'quicksilverscientific',
-              'marshalls',
-              'legendarywhitetails',
-              'solawave',
-              'kingschools',
-              'tripmasters',
-              'safelite',
-              'shopmaximumfitness',
-              'nasm',
-              'bloomchic',
-              'canadapharmacy',
-              'safcodental',
-              'albanypark'
-            ]
-            if (primarySitesCortney.includes(storeNameFinal)) {
-              insCortSheet.addToSheet(values);
-            } else {
-              insSheet.addToSheet(values);
-            }
-
+            insSheet.addToSheet(values);
             console.log('Store ' + storeNameFinal + ' added in DB');
-
           }
         });
       }
